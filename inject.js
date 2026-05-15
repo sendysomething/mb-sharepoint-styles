c7231b5f7625873ba6ebfd686ec3c8233f6144ef
sendysomething <sendy.moreno@munibilling.com>
2026-05-15 16:58:32 -0400

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,5 @@
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = 'https://raw.githubusercontent.com/sendysomething/mb-sharepoint-styles/refs/heads/main/custom.css';
+document.head.appendChild(link);
